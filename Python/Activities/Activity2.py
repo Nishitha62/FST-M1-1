0729b81c3c9a343012019f47a82160217410fdec
Nishitha Shetty <129272387+Nishitha62@users.noreply.github.com>
2023-04-12 12:47:55 +0530

    Add files via upload

---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,6 @@
+num=int(input("Enter a Number: "))
+mod=num%2
+if mod>0:
+    print("Number is a odd number")
+else:
+    print("Number is a Even number")
---
+++ Python/Activities/Activity21.py
@@ -0,0 +1,9 @@
+
+def test_division():
+
+    num1 = 100
+    num2 = 5
+
+    quot = num1 / num2
+
+    assert quot == 20
